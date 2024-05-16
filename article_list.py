5a3d52a51fd573a4f190c49909c2923b21049edc
connorli18 <115739603+connorli18@users.noreply.github.com>
2024-05-15 22:42:23 -0400

    got everything working

--- article_list.py
+++ article_list.py
@@ -1,8 +1,26 @@
-article_list = {
+math_article_list = {
     "FTCNotes": {
         "filename": "test.txt",
         "author": "Connor Li",
-        "title": "FTC Notes",
-        "publish-date": "2021-06-09"
+        "title": "Fundamental Theorem of Calculus",
+        "publish-date": "2024-03-01"
+    },
+    "parityprob": {
+        "filename": "theparityproblem.txt",
+        "author": "Connor Li",
+        "title": "Parity Problem In Sieve Theory",
+        "publish-date": "2024-04-26"
+    },
+    "brunssieve": {
+        "filename": "brunssieve.txt",
+        "author": "Connor Li",
+        "title": "Brun's Combinatorial Sieve & Twin Primes",
+        "publish-date": "2024-03-20"
+    },
+    "quantests": {
+        "filename": "quantests.txt",
+        "author": "Connor Li",
+        "title": "Quantitative Estimates & Sums of Two Cubes",
+        "publish-date": "2024-02-07"
     }
 }
--- server.py
+++ server.py
@@ -5,26 +5,33 @@ from flask import sessions
 from flask import abort
 import json
 import random
-from article_list import article_list
+from article_list import math_article_list
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
-
 @app.route('/articlespec/<article>')
 def articlespec(article):
     # Assuming article_list is a dictionary with article names as keys and filenames as values
-    if article not in article_list:
+    if article not in math_article_list:
         abort(404)
-    filename = article_list[article]['filename']
+    filename = math_article_list[article]['filename']
+    title = math_article_list[article]['title']
+    author = math_article_list[article]['author']
+
+    publish_date = math_article_list[article]['publish-date']
+    date = datetime.strptime(publish_date, '%Y-%m-%d')
+    formatted_date = date.strftime('%B %Y')
+
     if filename is None:
         abort(404)  # If the article is not found in the list, return a 404 error
     # Join the filename with the directory path
     full_path = os.path.join('latex_converter', filename)
     with open(full_path, 'r') as file:
         article_content = file.read()
-    return render_template('articlespec.html', content=article_content)
+    return render_template('articlespec.html', content=article_content, title=title, author=author, date=formatted_date)
 
 @app.route('/')
 def index():
@@ -40,9 +47,19 @@ def search():
     result = []
     return render_template('search.html', query=query, result=result)
 
-@app.route('/articles')
-def articles():
-    return render_template('articles.html')
+@app.route('/matharticles')
+def matharticles():
+    rec1 = "values"
+    rec2 = "values"
+    rec3 = "values"
+    return render_template('matharticles.html', rec1=rec1, rec2=rec2, rec3=rec3)
+
+@app.route('/csarticles')
+def csarticles():
+    rec1 = "values"
+    rec2 = "values"
+    rec3 = "values"
+    return render_template('csarticles.html', rec1=rec1, rec2=rec2, rec3=rec3)
 
 @app.route('/about')
 def about():
