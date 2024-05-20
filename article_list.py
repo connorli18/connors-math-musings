45b14277b45f0415a6f1aa269a03ef17d7be34cd
connorli18 <115739603+connorli18@users.noreply.github.com>
2024-05-19 22:41:48 -0400

    balls

--- article_list.py
+++ article_list.py
@@ -40,6 +40,36 @@ math_article_list = {
         "author": "Connor Li",
         "title": "K-Free Numbers: A Combinatorial Approach",
         "publish-date": "2024-02-28"
+    },
+    "realdirichar": {
+        "filename": "realdirichar.txt",
+        "author": "Connor Li",
+        "title": "Interesting Property of Real Dirichlet Characters",
+        "publish-date": "2024-05-02"
+    },
+    "introfunctions": {
+        "filename": "introfunctions.txt",
+        "author": "Connor Li",
+        "title": "Basic Introduction to Functions",
+        "publish-date": "2024-05-03"
+    },
+    "intalgreview": {
+        "filename": "intalgreview.txt",
+        "author": "Connor Li",
+        "title": "Intermediate Algebra Review",
+        "publish-date": "2024-05-19"
+    },
+    "nonvanishzeta": {
+        "filename": "nonvanishzeta.txt",
+        "author": "Connor Li",
+        "title": "Non-Vanishing Zeta Function for Re(s) > 1",
+        "publish-date": "2024-03-15"
+    },
+    "primeprog3k2": {
+        "filename": "primeprog3k2.txt",
+        "author": "Connor Li",
+        "title": "Prime Progression of the Form 3k+2",
+        "publish-date": "2024-04-05"
     }
 }
 
@@ -49,5 +79,23 @@ cs_article_list = {
         "author": "Connor Li, Heeyun Kim",
         "title": "IoT Firewall Research Report",
         "publish-date": "2024-05-13"
+    },
+    "discretep2": {
+        "filename": "discretep2.txt",
+        "author": "Connor Li",
+        "title": "Discrete Mathematics Final Cheat Sheet",
+        "publish-date": "2023-12-03"
+    },
+    "discretep1": {
+        "filename": "discretep1.txt",
+        "author": "Connor Li",
+        "title": "Discrete Mathematics Midterm Cheat Sheet",
+        "publish-date": "2023-10-13"
+    },
+    "dynatrout": {
+        "filename": "dynatrout.txt",
+        "author": "Connor Li",
+        "title": "Lake of Trout: A Dynamical System",
+        "publish-date": "2023-11-10"
     }
 }
--- server.py
+++ server.py
@@ -91,6 +91,13 @@ def csarticles():
 def about():
     return render_template('about.html')
 
+@app.route('/sitemap.xml', methods=['GET'])
+def sitemap():
+    try:
+        return app.send_static_file('sitemap.xml')
+    except FileNotFoundError:
+        abort(404)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
